47fc67f70ed695c82ba406407461e79ba786d8c5
Anya <anya.s.litvinova@gmail.com>
2019-11-26 11:06:10 -0500

    done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,13 +1,63 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from '../containers/YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  
+  state = {
+    allBots: [],
+    myBots: [],
+    selectedBot: false,
+    showBotSpecs: false
+  }
+
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(res => res.json())
+      .then(bots => this.setState({
+        ...this.state,
+        allBots: bots
+      }))
+      console.log(this.state)
+  }
+
+  renderMyBot = bot => {
+    const randomBots = this.state.myBots.slice()
+    if (this.state.myBots.includes(bot)) {
+      randomBots.splice(randomBots.indexOf(bot), 1)
+      this.setState({
+        ...this.state,
+        myBots: randomBots
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        showBotSpecs: true,
+        selectedBot: true
+      })
+      
+    }
+  }
+    
+
+    handleClick=()=>{
+      this.setState({
+        ...this.state,
+        showBotSpecs: false,
+        selectedBot: false
+      })
+      }
 
   render() {
+    console.log( this.state.myBots)
+    console.log( this.state.allBots)
     return (
       <div>
-        {/* put your components here */}
+      <YourBotArmy myBots={this.state.myBots} />
+       <BotCollection allBots={this.state.allBots} handleClick={this.handleClick}/>
       </div>
+      
     );
   }
 
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,18 +5,18 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+    console.log(this.props.myBots)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.props.myBots.length < 1 ? null : this.props.myBots.map(bot => <BotCard key={bot.id} bot={bot} />)}
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
